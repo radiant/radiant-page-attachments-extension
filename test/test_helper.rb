33579b03d32a6960e4e4608b6b00d433d309a1ca
Jason Garber <jg@jasongarber.com>
2009-06-11 14:13:07 -0400

    Remove extraneous test helper that doesn't work in Radiant 0.7.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,9 +15,6 @@ require "#{RADIANT_ROOT}/test/test_helper"
 
 class Test::Unit::TestCase
 
-  # Include a helper to make testing Radius tags easier
-  test_helper :extension_tags
-
   # Add the fixture directory to the fixture path
   self.fixture_path << File.dirname(__FILE__) + "/fixtures"
 
